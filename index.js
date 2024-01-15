d66e6832bdfd52e8cab909bb0303cf11e0db0cc3
Gustakx <gustavobruno12333@gmail.com>
2024-01-15 11:44:23 -0300

    1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {} from 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import http from 'http';
+import fs from 'fs';
 
 const app = express();
 const server = http.createServer(app);
@@ -17,6 +18,33 @@ app.use(express.static(path.join(__dirname, 'template')));
 
 app.get('/', (req, res) => {
     res.render('../template/index');
+});
+
+app.get('/video', (req, res) => {
+    res.render('../template/video');
+});
+
+app.get('/videoRender', (req, res) => {
+    const tamanho = req.headers.range;
+    if(!tamanho){
+        res.status(400).send('Error');
+    }
+    const videoPath = './template/uploads/video.mp4';
+    const videoSize = fs.statSync(videoPath).size;
+
+    const CHUNK_SIZE = 10 ** 6;
+    const start = Number(tamanho.replace(/\D/g, ''));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    const contentLength = end - start + 1;
+    const headers = {
+        'Content-Range' : `bytes ${start}-${end}/${videoSize}`,
+        'Accept-Ranges' : 'bytes',
+        'Content-Length' : contentLength,
+        'Content-Type' : 'video/mp4'
+    };
+    res.writeHead(206, headers);
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+    videoStream.pipe(res);
 });
 
 const users = {};
