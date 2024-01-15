a0047bb7ff3ad8dfc2e9feef90fa7a50fd451c48
Gustakx <gustavobruno12333@gmail.com>
2024-01-15 15:48:13 -0300

    1

--- upload/upload.js
+++ upload/upload.js
@@ -1,6 +1,5 @@
 import multer from 'multer';
 import path from 'path';
-import fs from 'fs';
 
 // Configuração de armazenamento
 const storage = multer.diskStorage({
@@ -10,7 +9,6 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
         // Extração da extensão do arquivo original:
         const extensaoArquivo = path.extname(file.originalname);
-        fs.unlink('./template/uploads/video.mp4')
         // Cria um código randômico que será o nome do arquivo
         const novoNomeArquivo = new Date().getTime();
 
