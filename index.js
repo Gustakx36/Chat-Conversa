9b3e0036643e4edf836184b4a192b33fc8d62169
Gustakx36 <gustavobruno12333@gmail.com>
2024-01-17 21:43:32 -0300

    1

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import { fileURLToPath } from 'url';
 import { Server } from 'socket.io';
 import {} from 'dotenv/config';
 import express from 'express';
-import upload from './upload/upload.js';
 import user from './entidades/user.js';
 import mensagem from './entidades/mensagem.js';
 import path from 'path';
 import http from 'http';
-import fs from 'fs';
 
 const app = express();
 const server = http.createServer(app);
@@ -21,41 +19,6 @@ app.use(express.static(path.join(__dirname, 'template')));
 
 app.get('/', (req, res) => {
     res.render('../template/index');
-});
-
-app.post('/img_upload', upload.array('file[]', 1), (req, res) => {
-    res.status(200).json({status : true});
-});
-
-app.get('/uploadVideo', (req, res) => {
-    res.render('../template/upload');
-});
-
-app.get('/video', (req, res) => {
-    res.render('../template/video');
-});
-
-app.get('/videoRender', (req, res) => {
-    const tamanho = req.headers.range;
-    if(!tamanho){
-        res.status(400).send('Error');
-    }
-    const videoPath = './template/uploads/video.mp4';
-    const videoSize = fs.statSync(videoPath).size;
-
-    const CHUNK_SIZE = 10 ** 6;
-    const start = Number(tamanho.replace(/\D/g, ''));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-    const contentLength = end - start + 1;
-    const headers = {
-        'Content-Range' : `bytes ${start}-${end}/${videoSize}`,
-        'Accept-Ranges' : 'bytes',
-        'Content-Length' : contentLength,
-        'Content-Type' : 'video/mp4'
-    };
-    res.writeHead(206, headers);
-    const videoStream = fs.createReadStream(videoPath, { start, end });
-    videoStream.pipe(res);
 });
 
 app.get('/mensagens/:me/:user/:nivel/:amais', async (req, res) => {
